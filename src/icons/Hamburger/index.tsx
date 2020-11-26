d13e2b4caab504d7ac990fe4e77597d743e149db
Clemente Serrano <clementeserranosutil@gmail.com>
2020-11-27 00:09:45 +0100

    enhancement(icons): Change hamburger icon

--- src/icons/Hamburger/index.tsx
+++ src/icons/Hamburger/index.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 
-import { MainContainer, Line } from "./styles";
+import { IVectorProps } from "../../types";
 
-function HamburguerIcon() {
+function HamburguerIcon({
+  height = "100px",
+  color = "currentColor",
+  style,
+}: IVectorProps) {
   return (
-    <MainContainer>
-      <Line />
-      <Line />
-      <Line />
-    </MainContainer>
+    <svg height={height} style={style} x="0px" y="0px" viewBox="0 0 100 100">
+      <g>
+        <path
+          fill={color}
+          d="M100,56.836H0V43.164h100V56.836z M100,17.085H0v13.672h100V17.085z M100,69.243H0v13.672h100V69.243z"
+        />
+      </g>
+    </svg>
   );
 }
 
