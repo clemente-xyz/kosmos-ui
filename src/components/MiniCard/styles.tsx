34f02188c4728c57bc3b369d8fe6e5256028d991
Clemente Serrano <clementeserranosutil@gmail.com>
2021-08-30 23:54:02 +0200

    styles(mini card): Adjust padding

--- src/components/MiniCard/styles.tsx
+++ src/components/MiniCard/styles.tsx
@@ -6,9 +6,9 @@ const MainContainer = styled.article<{ hasIcon: boolean }>`
   display: flex;
   align-items: ${({ hasIcon }) => (hasIcon ? "center" : "unset")};
   flex-direction: ${({ hasIcon }) => (hasIcon ? "row" : "column")};
-  padding: 32px 48px;
+  padding: 24px;
   background-color: ${theme.colorsPalette.white.default};
-  border-radius: 5px;
+  border-radius: 8px;
   box-shadow: 0px 0px 7px 0px rgba(0, 0, 0, 0),
     0 3px 5px 0px rgba(0, 0, 0, 0.03);
 
